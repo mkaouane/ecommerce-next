58b611d2985e89f0535a04cc7def02154707b74c
Mohamed <51891244+mkaouane@users.noreply.github.com>
2022-05-24 11:51:57 +0200

    rename lib

--- pages/success.js
+++ pages/success.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import {BsBagCheckFill} from 'react-icons/bs'
 // import {useRouter} from 'next/router';
 import {useStateContext} from '../context/StateContext';
-import {runFireworks} from '../LIB/utils';
+import {runFireworks} from '../lib/utils';
 
 
 
